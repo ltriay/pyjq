6384d7f8a5b43489449e9a0ec95f4fa34fbc6b2c
Goudargues <goudargues@zapataz.net>
2018-01-28 23:20:44 +0100

    Managing more types

--- pyjq/pyjq.py
+++ pyjq/pyjq.py
@@ -18,36 +18,37 @@ def return_value(value, ret):
 def lookup(struc, struc_search, default=[]):
     ret = default
 
-    if type(struc) is not type(struc_search):
-        raise TypesNotInSync
-
     if type(struc_search) is dict:
+        if type(struc) is not dict:
+            raise TypesNotInSync
         for key, value in struc_search.items():
             if key in struc:
-                if issubclass(Inspect, type(value)):
+                if issubclass(type(value), Inspect):
                     ret = value.get(struc[key], ret)
                 else:
                     ret = lookup(struc[key], struc_search[key], ret)
-    elif type(struc) is list:
+    elif type(struc_search) in [str, int, float, complex, type(None)]:
+        ret.append(struc_search)
+    elif "__getitem__" in struc_search.__dir__():
+        if "__getitem__" not in struc.__dir__():
+            raise TypesNotInSync
         if len(struc_search) == 1:
             item = struc_search[0]
             for an_item in struc:
-                if issubclass(Inspect, type(item)):
+                if issubclass(type(item), Inspect):
                     ret = item.get(an_item, ret)
                 else:
                     ret = lookup(an_item, item, ret)
         else:
             raise TypesNotInSync
     else:
+        print(struc, struc_search)
         raise TypeError
 
     return ret
 
 
 class Inspect:
-    def __init__(self, sub_listed=None):
-        self.sub_listed = sub_listed
-
     def get(self, value, ret):
         ret.append(value)
         return ret
@@ -57,21 +58,34 @@ class Inspect:
 CreateList = Inspect
 
 
-class CreateDict(Inspect):
-    def __init__(self, sub_dir=None, sub_values=None):
-        self.sub_dir = sub_dir
-        self.sub_values = sub_values
+class MapDict(Inspect):
+    def __init__(self, to_map):
+        self.map = to_map
 
     def get(self, value, ret):
-        pass
+        ret.append({k: v for k, v in zip(value, self.map)})
+        return ret
 
+
+class Caller:
+    """Differ call of a callable"""
+    def __init__(self, caller):
+        self.caller = caller
+
+    def get(self, *args, **kwargs):
+        return self.caller(*args, **kwargs)
+
+
+def a_callable(x, y):
+    return x * y
+
+
+SAMPLE = {"a1": [1, 2, 3]}
+SEARCH = {"a1": CreateList()}
 
 EXAMPLE = {"a": 1, "b": [{"c": ["i", "j"]}, {"d": {"o": ["p", "q"]}}, {"e": 8}], "k": 9, "l": {"m": 10},
-           "n": [11, 12, 13], "r": [20, 21, 22]}
-
-LOOKUP = {"b": [{"c": Inspect(), "d": Inspect()}], "l": Inspect(), "n": [Inspect()],
-          "r": Inspect([Inspect()])}
-
-print(LOOKUP)
+           "n": (11, 12, 13), "r": [20, 21, 22], "f": "toto", "p": [1, 2]}
+LOOKUP = {"b": [{"c": CreateList(), "d": CreateList()}], "l": CreateList(), "n": [CreateList()],
+          "r": CreateList(), "f": "const", "p": MapDict(["a", "b"])}
 
 print(lookup(EXAMPLE, LOOKUP))
